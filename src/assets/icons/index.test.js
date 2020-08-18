24ca57a0ce84f9780dbf22dea17d0e804a905334
Victor Miranda <vmmelo1@gmail.com>
2020-08-18 10:56:10 -0300

    melhorando coverage

--- src/assets/icons/index.test.js
+++ src/assets/icons/index.test.js
@@ -1,6 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
 import { Instagram, Whatsapp } from '.'
 
-it('should have the right type', () => {
+it('should render Instagram svg', () => {
   expect(typeof Instagram).toBe('function')
+  const div = document.createElement('div');
+  ReactDOM.render(<Instagram />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should render whatsapp svg', () => {
   expect(typeof Whatsapp).toBe('function')
+  const div = document.createElement('div');
+  ReactDOM.render(<Whatsapp />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
